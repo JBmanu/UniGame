c115f0a9812c8332c73144bcd5214ec49af93df6
Totti00 <gek5800@gmail.com>
2021-12-22 19:17:50 +0100

    Devo finire login. Per ora concluso index

--- js/click.js
+++ js/click.js
@@ -1,126 +1,116 @@
 $(document).ready(function(){
-    var slides = document.querySelectorAll('.slide');
-    var btns = document.querySelectorAll('.btn');
-    let currentSlide = 1;
-  
-    /*$(document).ready(function(){
-        $("#menuButton").click(function(){
-          $("#menu").slideToggle();
-          document.getElementById("menu").style.position = "relative";
-        }); 
-      });*/
 
-      $(".icon").click(function(){
-        var x = document.getElementById("myTopnav");
-        if (x.className === "topnav") {
-          x.className += " responsive";
-        } else {
-          x.className = "topnav";
-        }
-      });
+  var slides = document.querySelectorAll('.slide');
+  var btns = document.querySelectorAll('.btn');
+  let currentSlide = 1;
 
-
-
-
-    $("#submit").click(function() {
-        if ($("#search").val() === "Search...")
-        {
-            alert("Inserire un valore valido");
-            return false;
-        }
-    });
-    
-    $("#search").blur(function() {
-        if(this.value == ''){ 
-            this.value = 'Search...';
-        }
-    });
-
-    $("#login-name").blur(function() {
-        if(this.value == ''){ 
-            this.value = 'Nome';
-        }
-    });
-
-    $("#login-surname").blur(function() {
-        if(this.value == ''){ 
-            this.value = 'Cognome';
-        }
-    });
-
-    $("#login-mail").blur(function() {
-        if(this.value == ''){ 
-            this.value = 'E-mail';
-        }
-    });
-
-    $("#search").focus(function() {
-        if (this.value == 'Search...') {
-            this.value = ''; 
-        }
-    });
-
-    $("#login-name").focus(function() {
-        if (this.value == 'Nome') {
-            this.value = ''; 
-        }
-    });
-
-    $("#login-surname").focus(function() {
-        if (this.value == 'Cognome') {
-            this.value = ''; 
-        }
-    });
-
-    $("#login-mail").focus(function() {
-        if (this.value == 'E-mail') {
-            this.value = ''; 
-        }
-    });
-
-    var manualNav = function(manual){
-        slides.forEach((slide) => {
-          slide.classList.remove('active');
-          btns.forEach((btn) => {
-            btn.classList.remove('active');
-          });
-        });
-        slides[manual].classList.add('active');
-        btns[manual].classList.add('active');
+  $(".icon").click(function(){
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+    x.className = "topnav";
     }
+  });
 
-    btns.forEach((btn, i) => {
-        btn.addEventListener("click", () => {
-          manualNav(i);
-          currentSlide = i;
-        });
+  $("#submit").click(function() {
+    if ($("#search").val() === "Search...") {
+      alert("Inserire un valore valido");
+      return false;
+    }
+  });
+    
+  $("#search").blur(function() {
+    if(this.value == ''){ 
+      this.value = 'Search...';
+    }
+  });
+
+  $("#login-name").blur(function() {
+    if(this.value == ''){ 
+      this.value = 'Nome';
+    }
+  });
+
+  $("#login-surname").blur(function() {
+    if(this.value == ''){ 
+      this.value = 'Cognome';
+    }
+  });
+
+  $("#login-mail").blur(function() {
+    if(this.value == ''){ 
+      this.value = 'E-mail';
+    }
+  });
+
+  $("#search").focus(function() {
+    if (this.value == 'Search...') {
+      this.value = ''; 
+    }
+  });
+
+  $("#login-name").focus(function() {
+    if (this.value == 'Nome') {
+    this.value = ''; 
+    }
+  });
+
+  $("#login-surname").focus(function() {
+    if (this.value == 'Cognome') {
+      this.value = ''; 
+    }
+  });
+
+  $("#login-mail").focus(function() {
+    if (this.value == 'E-mail') {
+        this.value = ''; 
+    }
+  });
+
+  var manualNav = function(manual){
+    slides.forEach((slide) => {
+      slide.classList.remove('active');
+      btns.forEach((btn) => {
+        btn.classList.remove('active');
+      });
     });
+    slides[manual].classList.add('active');
+    btns[manual].classList.add('active');
+  }
 
-    var repeat = function(activeClass){
-        let active = document.getElementsByClassName('active');
-        let i = 1;
+  btns.forEach((btn, i) => {
+    btn.addEventListener("click", () => {
+      manualNav(i);
+      currentSlide = i;
+    });
+  });
+
+  var repeat = function(activeClass){
+    let active = document.getElementsByClassName('active');
+    let i = 1;
   
-        var repeater = () => {
-          setTimeout(function(){
-            [...active].forEach((activeSlide) => {
-              activeSlide.classList.remove('active');
-            });
+    var repeater = () => {
+      setTimeout(function(){
+        [...active].forEach((activeSlide) => {
+          activeSlide.classList.remove('active');
+        });
   
-          slides[i].classList.add('active');
-          btns[i].classList.add('active');
-          i++;
+        slides[i].classList.add('active');
+        btns[i].classList.add('active');
+        i++;
   
-          if(slides.length == i){
-            i = 0;
-          }
-          if(i >= slides.length){
-            return;
-          }
-          repeater();
-        }, 10000);
+        if(slides.length == i){
+          i = 0;
+        }
+        if(i >= slides.length){
+          return;
         }
         repeater();
+      }, 10000);
     }
-    repeat();
+    repeater();
+  }
+  repeat();
 });
 
