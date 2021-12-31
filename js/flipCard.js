ba3ed4ce98a862c2d61c861e4907ea599277b018
Manuel Buizo <65312924+JBmanu@users.noreply.github.com>
2021-12-31 14:05:32 +0100

    add pop up of cart page

--- js/flipCard.js
+++ js/flipCard.js
@@ -55,20 +55,44 @@ cartlistCard.forEach(el => {
 
         if(parseInt(quantity.textContent) < 1 ) {
             el.animate({ transform: 'scale(0.2, 0.2)' }, 400);
-            setTimeout(function () { el.remove() }, 400);
+            setTimeout(function () { el.remove() }, 350);
         }
     });
 
 });
 
 var btnPay = document.querySelector('.cntnr_down_btn');
-var cntnBody = document.querySelector('.cntn_master_body');
+var cntnBody = document.querySelector('body');
 var cntnrCard = document.querySelector('.cntnr_list_card');
 
 btnPay.addEventListener('click', () => {
+    window.scrollTo(0,0);
+
     cntnrCard.style.filter = 'blur(10px)';
     btnPay.style.filter = 'blur(10px)';
+    
+    cntnBody.innerHTML += `
+    <div class="cntnr_pay  bg_color_secondary  curve_obj_h50">
+        <strong class="title_pay  font_secondary  fg_color_primary ">Metodo Di Pagamento</strong>
+        <form class="cntnr_form_method_pay  space_top_margin_big  font_secondary  fg_color_third" action="">
+            <label class="radio_btn_pay ">
+                <input class="circle_obj  space_bottom_margin_card bg_color_third  radio_circle" type="radio" id="usedItem" name="pay" value="used"/>
+                <span>MasterCard</span> 
+            </label>
+            <label class="radio_btn_pay">
+                <input class="circle_obj  space_bottom_margin_card  bg_color_third  radio_circle" type="radio" id="newItem" name="pay" value="new" />
+                <span>PayPal</span> 
+            </label>
+            <label class="radio_btn_pay">
+                <input class="circle_obj  space_bottom_margin_card  bg_color_third  radio_circle" type="radio" id="ps4Item" name="pay" value="ps4"/>
+                <span>Bancomat</span> 
+            </label>
+            <label class="radio_btn_pay">
+                <input class="circle_obj  space_bottom_margin_card  bg_color_third  radio_circle" type="radio" id="ps5Item" name="pay" value="ps5" checked="checked"/>
+                <span>American Express</span> 
+            </label>
+        </form>
+        <input class="submit_method_pay  space_top_margin_big  curve_obj_h15  font_primary  fg_color_third" type="submit" value="Conferma"/>
+    </div>`;
 
-
-    // cntnBody.appendChild(document.createElement('div'))
 });
