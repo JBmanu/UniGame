b02c9b481055ecc53c1af13e7c90def2549aa934
Totti00 <gek5800@gmail.com>
2022-01-14 17:33:35 +0100

    add click.js

--- js/click.js
+++ js/click.js
@@ -1,27 +1,14 @@
 $(document).ready(function(){
 
-  var slides = document.querySelectorAll('.slide');
-  var btns = document.querySelectorAll('.btn');
-  let currentSlide = 1;
-
   //Controlla il menu dell'header delle pagine
   $(".icon").click(function(){      
-    var x = document.getElementById("myTopnav");
+    let x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
       x.className += " responsive";
     } else {
     x.className = "topnav";
     }
   });
-
-  //Controlla il search, che il campo sia diverso da vuoto
-  $("#submit").click(function() {
-    if ($("#search").val() === "Search") {
-      alert("Inserire un valore valido");
-      return false;
-    }
-  });
-  
 
   //Se il campo è vuoto riscrive Nome nel form registrazione
   $("#login-name").blur(function() {
@@ -34,13 +21,6 @@ $(document).ready(function(){
   $("#login-surname").blur(function() {
     if(this.value == ''){ 
       this.value = 'Cognome';
-    }
-  });
-
-  //Quando si clicca su search, toglie la parola Search
-  $("#search").focus(function() {
-    if (this.value == 'Search...') {
-      this.value = ''; 
     }
   });
 
@@ -57,50 +37,5 @@ $(document).ready(function(){
       this.value = ''; 
     }
   });
-
-  var manualNav = function(manual){
-    slides.forEach((slide) => {
-      slide.classList.remove('active');
-      btns.forEach((btn) => {
-        btn.classList.remove('active');
-      });
-    });
-    slides[manual].classList.add('active');
-    btns[manual].classList.add('active');
-  }
-
-  btns.forEach((btn, i) => {
-    btn.addEventListener("click", () => {
-      manualNav(i);
-      currentSlide = i;
-    });
-  });
-
-  var repeat = function(activeClass){
-    let active = document.getElementsByClassName('active');
-    let i = 1;
-  
-    var repeater = () => {
-      setTimeout(function(){
-        [...active].forEach((activeSlide) => {
-          activeSlide.classList.remove('active');
-        });
-  
-        slides[i].classList.add('active');
-        btns[i].classList.add('active');
-        i++;
-  
-        if(slides.length == i){
-          i = 0;
-        }
-        if(i >= slides.length){
-          return;
-        }
-        repeater();
-      }, 10000);
-    }
-    repeater();
-  }
-  repeat();
 });
 
