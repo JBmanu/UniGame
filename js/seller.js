a5cde7fe93075af394e8c25a0cb7f025cf0432ca
Manuel Buizo <65312924+JBmanu@users.noreply.github.com>
2022-01-16 12:44:55 +0100

    finish rotate arrow

--- js/seller.js
+++ js/seller.js
@@ -6,34 +6,19 @@ function hideElement(element){
 
 $(document).ready(function(){
 
-    let bntsSeller = document.querySelectorAll('.accordion > button');
-    let arrowExpanse = document.querySelectorAll('button > .expand_button');
-
-
-    bntsSeller.forEach(el => {
-        let indexArrow = 0;
-        el.onclick = () => {
-            console.log("cliccooo");
-
-            if( arrowExpanse[indexArrow].style.transform == 'rotate(90deg)' ) {
-                arrowExpanse[indexArrow].style.transform = 'rotate(0deg)';
-            } else {
-                arrowExpanse[indexArrow].style.transform = 'rotate(90deg)';
-            }
-            indexArrow += 1;
-        }; 
-    });
-
     $("button").click(function(){
         if($(this).hasClass("selected")){
             //bottone già cliccato, allora lo nascondo
             hideElement($(this));
+            $(this).children(".expand_button").css("transform", "rotate(0deg)");
         }
         else{
             //bottone non cliccato, nascondere eventualmente elemento precedentemente cliccato e visulizzare contenuto div che segue
             hideElement($("button.selected"));
             $(this).addClass("selected");
             $(this).next().slideDown();
+
+            $(this).children(".expand_button").css("transform", "rotate(90deg)");
         }
     });
 });
