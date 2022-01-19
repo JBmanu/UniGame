6d6cebe74358d541c08dfe6387e984fed656534f
Totti00 <gek5800@gmail.com>
2022-01-19 16:47:46 +0100

    Aggiunto notifiche in ogni pagina e creato notify.js

--- js/click.js
+++ js/click.js
@@ -1,25 +1,3 @@
-toastr.options = {
-  "closeButton": true,
-  "debug": false,
-  "newestOnTop": false,
-  "progressBar": false,
-  "positionClass": "toast-top-right",
-  "onclick": null,
-  "showDuration": "500",
-  "hideDuration": "1000",
-  "timeOut": "5000",
-  "extendedTimeOut": "1000",
-  "showEasing": "swing",
-  "hideEasing": "linear",
-  "showMethod": "fadeIn",
-  "hideMethod": "fadeOut"
-}
-
-function notify(){
-  toastr.info("Controllare pagina notifiche per info","L'ordine #2345 e' stato spedito!");
-}
-
-
 $(document).ready(function(){
 
   //Controlla il menu dell'header delle pagine
@@ -31,40 +9,5 @@ $(document).ready(function(){
     x.className = "topnav";
     }
   });
-
-  //Se il campo è vuoto riscrive Nome nel form registrazione
-  $("#login-name").blur(function() {
-    if(this.value == ''){ 
-      this.value = 'Nome';
-    }
-  });
-
-  //Se il campo è vuoto riscrive Cognome nel form registrazione
-  $("#login-surname").blur(function() {
-    if(this.value == ''){ 
-      this.value = 'Cognome';
-    }
-  });
-
-  //Quando si clicca su Nome nel form registrazione, toglie la parola Nome
-  $("#login-name").focus(function() {
-    if (this.value == 'Nome') {
-    this.value = ''; 
-    }
-  });
-
-    //Quando si clicca su Cognome nel form registrazione, toglie la parola Cognome
-  $("#login-surname").focus(function() {
-    if (this.value == 'Cognome') {
-      this.value = ''; 
-    }
-  });
-
-
-
-setInterval("notify()", 8000);
-
-
-
 });
 
---
+++ js/notify.js
@@ -0,0 +1,25 @@
+toastr.options = {
+    "closeButton": true,
+    "debug": false,
+    "newestOnTop": false,
+    "progressBar": true,
+    "positionClass": "toast-top-right",
+    "preventDuplicates": true,
+    "onclick": null,
+    "showDuration": "500",
+    "hideDuration": "1000",
+    "timeOut": "5000",
+    "extendedTimeOut": "1000",
+    "showEasing": "swing",
+    "hideEasing": "linear",
+    "showMethod": "fadeIn",
+    "hideMethod": "fadeOut"
+}
+  
+function notify(){
+    toastr.info("Controllare pagina notifiche per info","L'ordine #2345 e' stato spedito!");
+}
+
+$(document).ready(function(){
+    setInterval("notify()", 8000);
+});
